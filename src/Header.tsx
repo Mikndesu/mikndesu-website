90a34c14acc9b3b14551fa2d07e5a9a22e735ce2
Mikndesu <mi22kknsinge@ezweb.ne.jp>
2022-07-19 03:51:37 +0900

    adjust position

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,13 +1,13 @@
-import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu"
 import { margin } from "@mui/system";
 
 const Header = () => {
     return (
-        <Box sx={{flexGrow: 1}}>
-            <AppBar position="static" style={{backgroundColor:"#708090"}}>
+        <Box>
+            <AppBar position="static" style={{ backgroundColor: "#708090" }}>
                 <Toolbar>
-                    <Typography variant="h6" component="div" sx={{flexGrow:1, paddingLeft:"300px"}}>
+                    <Typography variant="h6" component="div" sx={{margin:"200"}}>
                         Mikndesu's Mods
                     </Typography>
                 </Toolbar>
