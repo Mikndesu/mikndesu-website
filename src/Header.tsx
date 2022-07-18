4b82485a66fef9ec8005593d99fbe92a12879507
Mikndesu <mi22kknsinge@ezweb.ne.jp>
2022-07-19 00:13:27 +0900

    set background color

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,14 +1,19 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
-import React from "react";
+import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu"
+import { margin } from "@mui/system";
 
 const Header = () => {
     return (
-        <AppBar position="static">
-            <Toolbar>
-                <Typography>Mikndesu's Mods</Typography>
-            </Toolbar>
-        </AppBar>
-    );
+        <Box sx={{flexGrow: 1}}>
+            <AppBar position="static" style={{backgroundColor:"#708090"}}>
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{flexGrow:1, paddingLeft:"300px"}}>
+                        Mikndesu's Mods
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    )
 }
 
 export default Header;
